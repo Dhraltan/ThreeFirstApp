c32ec091f2819f7b14f4c8a0479afbcfbb6b611a
Gheorghe Catalin <catalin.gheorghe@cst.ro>
2021-04-16 16:45:35 +0300

    Changed the style for welcome text

--- src/app/features/home/components/welcome-text/welcome-text.component.ts
+++ src/app/features/home/components/welcome-text/welcome-text.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 @Component({
   selector: 'app-welcome-text',
@@ -22,13 +23,13 @@ export class WelcomeTextComponent implements OnInit {
 
   ngOnInit(): void {
     this.initializeRenderer();
-    this.animate()
+    this.animate();
   }
 
   initializeRenderer(): void {
     this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color(0x000000);
-    this.scene.fog = new THREE.Fog(0x000000, 250, 1400);
+    this.scene.background = new THREE.Color(0x00263d);
+    this.scene.fog = new THREE.Fog(0x00263d, 500, 1500);
 
     this.container = document.getElementById('welcome-text-container');
     console.log(this.container.clientWidth);
@@ -37,9 +38,9 @@ export class WelcomeTextComponent implements OnInit {
       75,
       this.container.clientWidth / this.container.clientHeight,
       0.1,
-      2000
+      3000
     );
-    this.camera.position.set(0, 200, 600);
+    this.camera.position.set(0, 150, 500);
     this.cameraTarget = new THREE.Vector3(0, 150, 0);
 
     const dirLight = new THREE.DirectionalLight(0xffffff, 0.125);
@@ -50,24 +51,22 @@ export class WelcomeTextComponent implements OnInit {
     pointLight.position.set(0, 300, 90);
     this.scene.add(pointLight);
 
-    
-
     const plane = new THREE.Mesh(
       new THREE.PlaneGeometry(10000, 10000),
       new THREE.MeshBasicMaterial({
-        color: 0x0b9959,
-        opacity: 0.5,
-        transparent: true,
+        color: 0x087243,
+        opacity: 0.6,
+        transparent: false,
       })
     );
-    plane.position.y = 0;
+    plane.position.y = -200;
     plane.rotation.x = -Math.PI / 2;
     this.scene.add(plane);
 
 
-
     const group = new THREE.Group();
     group.position.y = 150;
+    group.position.x = -50;
 
     this.scene.add(group);
 
@@ -76,28 +75,30 @@ export class WelcomeTextComponent implements OnInit {
     loader.load(
       'assets/fonts/helvetiker_regular.typeface.json',
       function (font) {
-        const textGeo = new THREE.TextGeometry('      Welcome to the \n Three View Application', {
-          font: font,
+        const textGeo = new THREE.TextGeometry(
+          '      Welcome to the \n Three View Application',
+          {
+            font: font,
 
-          size: 90,
-          height: 20,
-          curveSegments: 4,
+            size: 90,
+            height: 10,
+            curveSegments: 4,
 
-          bevelThickness: 2,
-          bevelSize: 1.5,
-          bevelEnabled: true,
-        });
+            bevelThickness: 2,
+            bevelSize: 1.5,
+            bevelEnabled: true,
+          }
+        );
 
         textGeo.computeBoundingBox();
 
         const centerOffset =
           -0.5 * (textGeo.boundingBox.max.x - textGeo.boundingBox.min.x);
 
-
-          const materials = [
-            new THREE.MeshPhongMaterial({ color: 0xffffff, flatShading: true }), // front
-            new THREE.MeshPhongMaterial({ color: 0xffffff }), // side
-          ];
+        const materials = [
+          new THREE.MeshPhongMaterial({ color: 0x99d8ff, flatShading: true }), // front
+          new THREE.MeshPhongMaterial({ color: 0xade0ff }), // side
+        ];
         const textMesh1 = new THREE.Mesh(textGeo, materials);
 
         textMesh1.position.x = centerOffset;
@@ -123,6 +124,9 @@ export class WelcomeTextComponent implements OnInit {
     );
     this.renderer.setClearColor('#e5e5e5');
 
+    const controls = new OrbitControls(this.camera, this.renderer.domElement);
+    controls.update();
+
     window.addEventListener('resize', () => this.onWindowResize(), false);
   }
 
@@ -133,8 +137,8 @@ export class WelcomeTextComponent implements OnInit {
 
     this.camera.lookAt(this.cameraTarget);
 
-  this.renderer.clear();
-  this.renderer.render(this.scene, this.camera);
+    this.renderer.clear();
+    this.renderer.render(this.scene, this.camera);
   }
 
   onWindowResize() {
